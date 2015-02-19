6954b7bb83c1f3433ce185514596495f3c8d717d
Daniel Tufcea <d.tufcea@gmail.com>
2015-02-19 01:55:40 -0500

    Added required and optional tags

--- OBOFormat.py
+++ OBOFormat.py
@@ -9,20 +9,43 @@ supported_stanzas = ['[Term]', '[Instance]', '[Typedef]']
 
 class Header:
     tags = {}
-    type = 'header'
+    type = 'Header'
+    required_tags = []
+    optional_tags = []
 
     def __init__(self):
-        pass
+        self.set_required_tags()
+        self.set_optional_tags()
 
     def add_tag(self, key, val):
         self.tags[key] = val
 
+    def __repr__(self):
+        # TODO: print in the proper order according to OBO standard
+        representation = str(self.type) + "\n"
+        for key, val in self.tags.items():
+            representation += key + ": " + str(val) + "\n"
+        return representation
+
+    def set_required_tags(self):
+        self.required_tags = ['format-version']
+
+
+    def set_optional_tags(self):
+        self.optional_tags = ['data-version', 'data', 'saved-by', 'auto-generated-by', 'subsetdef', 'import',
+                                  'synonymtypedef', 'idspace', 'default-relationship-id-prefix', 'id-mapping',
+                                  'remark']
+
 class Stanza:
     tags = {}
     type = None
+    required_tags = []
+    optional_tags = []
 
     def __init__(self, type_, key=None, id_value=None):
         self.type = type_
+        self.update_required_tags()
+        self.update_optional_tags()
         if key == "id" and Stanza.is_valid_id:
             self.tags[key] = id_value
 
@@ -41,3 +64,51 @@ class Stanza:
             return False
         else:
             return True
+
+    def __repr__(self):
+        # TODO: print in the proper order according to OBO standard
+        representation = str(self.type) + "\n"
+        for key, val in self.tags.items():
+            representation += key + ": " + str(val) + "\n"
+        return representation
+
+    def update_required_tags(self):
+        if self.type == 'Term':
+            self.required_tags = ['id', 'name']
+        elif self.type == 'Typedef':
+            self.required_tags = ['id', 'name']
+        elif self.type == "Instance":
+            self.required_tags = ['id', 'name', 'instance_of']
+
+    def update_optional_tags(self):
+        if self.type == 'Term':
+            self.optional_tags = ['is_anonymous', 'alt_id', 'def', 'comment', 'subset', 'synonym', 'exact_synonym',
+                                  'narrow_synonym', 'broad_synonym', 'xref', 'xref_analog', 'xref_unk', 'is_a',
+                                  'intersection_of', 'union_of', 'disjoint_from', 'relationship', 'is_obsolete',
+                                   'replaced_by', 'consider', 'created_by', 'creation_date']
+        elif self.type == 'Typedef':
+            self.optional_tags = ['is_anonymous', 'alt_id', 'def', 'comment', 'subset', 'synonym', 'exact_synonym',
+                                  'narrow_synonym', 'broad_synonym', 'xref', 'xref_analog', 'xref_unk', 'is_a',
+                                  'relationship', 'is_obsolete', 'replaced_by', 'consider', 'created_by',
+                                  'creation_date', 'domain', 'range', 'inverse_of', 'transitive_over', 'is_cyclic',
+                                  'is_reflexive', 'is_symmetric', 'is_anti_symmetric', 'is_transitive', 'is_metadata_tag',
+                                  ]
+        elif self.type == "Instance":
+            self.optional_tags = ['property_value', 'is_anonymous', 'namespace', 'alt_id', 'comment', 'xref', 'synonym',
+                                  'is_obsolete', 'replaced_by', 'consider']
+
+class Validator():
+    required_tags = []
+    optional_tags = []
+    type = None
+
+    def __init__(self, obo_object):
+        self.type = obo_object.type
+        self.required_tags = obo_object.required_tags
+        self.optional_Tags = obo_object.optional_tags
+
+class ErrorInvalidID(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
--- Parser.py
+++ Parser.py
@@ -18,7 +18,8 @@ def parse_obo(filename):
         filename: The filename to read
     """
     output = [[] for _ in range(len(OBOFormat.supported_stanzas) + 2)]
-
+    current_stanza = None
+    current_header = None
     with open(filename, "r") as infile:
         current_header = OBOFormat.Header()  # header is read first
         current_stanza = None  # header is read first
@@ -31,18 +32,27 @@ def parse_obo(filename):
                 if current_header:
                     output[0] = current_header
                     current_header = None
-                else:
-                    output[1 + OBOFormat.supported_stanzas.index(line)].append(current_stanza)
+                elif current_stanza:
+                    type_ = "["+current_stanza.type+"]"
+                    if type_ in OBOFormat.supported_stanzas:
+                        output[1 + OBOFormat.supported_stanzas.index(type_)].append(current_stanza)
+                    else:
+                        output[1 + len(OBOFormat.supported_stanzas)].append(current_stanza)
                     current_stanza = None
 
                 current_stanza = OBOFormat.Stanza(line[1:-1])
             elif isinstance(line, basestring) and line[0] == "[" and line[-1] == "]":
-                # is unsupported stanza
+                # is unsupported <stanza>
+
                 if current_header:
                     output[0] = current_header
                     current_header = None
-                else:
-                    output[1 + len(OBOFormat.supported_stanzas)].append(current_stanza)
+                elif current_stanza:
+                    type_ = "["+current_stanza.type+"]"
+                    if type_ in OBOFormat.supported_stanzas:
+                        output[1 + OBOFormat.supported_stanzas.index(type_)].append(current_stanza)
+                    else:
+                        output[1 + len(OBOFormat.supported_stanzas)].append(current_stanza)
                     current_stanza = None
 
                 current_stanza = OBOFormat.Stanza(line[1:-1])
@@ -56,7 +66,6 @@ def parse_obo(filename):
                         current_header.add_tag(key, val)
                     if current_stanza:
                         current_stanza.add_tag(key, val)
-
         # Return header or last stanza
         if current_header is not None:
             output[0] = current_header
@@ -65,11 +74,9 @@ def parse_obo(filename):
             type_ = "["+current_stanza.type+"]"
             if type_ in OBOFormat.supported_stanzas:
                 output[1 + OBOFormat.supported_stanzas.index(type_)].append(current_stanza)
-                current_stanza = None
             else:
                 output[1 + len(OBOFormat.supported_stanzas)].append(current_stanza)
-                current_stanza = None
-
+            current_stanza = None
     return output
 
 
@@ -87,4 +94,4 @@ if __name__ == "__main__":
         print len(stanza_type)
         for stanza in stanza_type:
             stanza_counter += 1
-    print "Found %d GO terms" % stanza_counter
+    print "Found %d objects" % stanza_counter
